fd18f33bb9fb1cb2adb1baea4886d8739321490a
Luke Griffith <lukegriffith@Lukes-MacBook-Pro.local>
2019-02-10 11:00:35 -0500

    displaying runs WIP

--- src/runs.js
+++ src/runs.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { Navigation } from 'react-native-navigation';
+import firebase from 'react-native-firebase';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     Navigation.events().bindComponent(this);
+    // this.ref = firebase.firestore().collection('runs');
+    // this.unsubscribe = null;
   }
 
   state = {
+    runs: []
   }
 
 
@@ -25,12 +29,29 @@ export default class Home extends Component {
     });
   }
 
+
+  async componentDidAppear() {
+    let uid = firebase.auth().currentUser.uid;
+    let runs = [];
+    let fromDb = await firebase.firestore().collection('runs').where('uid', '==', uid).get()
+    fromDb.forEach((doc) => runs.push(doc.data()))
+    console.log('boi::::', runs)
+    this.setState({ runs });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Run History</Text>
         <TouchableOpacity onPress={() => Navigation.pop(this.props.componentId)}>
           <Text style={styles.welcome}>Back</Text>
+          {
+            this.state.runs.map(r => {
+              return (
+                <Text>{r.duration}</Text>
+              )
+            })
+          }
         </TouchableOpacity>
       </View>
     );
